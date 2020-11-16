217336a3c1c26b10fbe4cf91de0dacd91c53686c
Rosano Coutinho <pub@xeari.com>
2020-11-16 14:03:01 -0300

    Run npm install with package bin

--- package-bin.js
+++ package-bin.js
@@ -1,2 +1,17 @@
 #!/usr/bin/env node
 
+const mod = {
+
+	// LIFECYCLE
+
+	LifecycleScriptDidLoad() {
+		require('./main.js').OLSKBundleClearPackageLock();
+
+		require('child_process').spawn('npm', ['install', '--no-save'], {
+			stdio: 'inherit',
+		});
+	},
+
+};
+
+mod.LifecycleScriptDidLoad();
