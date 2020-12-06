11d09cace092181b651ccdba567578420375f2e0
Rosano Coutinho <pub@xeari.com>
2020-12-06 14:47:40 -0300

    Extract olsk-bundle cleanse

--- package-bin.js
+++ package-bin.js
@@ -2,9 +2,32 @@
 
 const mod = {
 
+	// CONTROL
+
+	ControlCleanse () {
+		if (process.env.NODE_ENV !== 'production') {
+			return;
+		}
+
+		return require('glob').sync(`+(${ [
+			'OLSK*',
+			'ROCO*',
+			'launchlet',
+			'MassageTXT',
+		].join('|') })/`, {
+			matchBase: true,
+			cwd: require('path').join(process.cwd(), 'node_modules'),
+			realpath: true,
+		}).forEach(require('OLSKDisk').OLSKDiskDeleteFolder);
+	},
+
 	// LIFECYCLE
 
 	LifecycleScriptDidLoad() {
+		if (process.argv[2] === 'cleanse') {
+			return mod.ControlCleanse(process.argv.slice(2), true);
+		}
+
 		require('./main.js').OLSKBundleClearPackageLock();
 
 		require('./main.js').OLSKBundleCopyEnvSample();
